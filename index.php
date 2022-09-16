c09539dadf9c4ac2eb8f6ba994ca870c17f1f148
Matus Krchnik <matus.krchnik@gmail.com>
2022-09-16 10:56:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $csv = CsvParser::getInstance("db.csv", false);
 
 $sentimentAnalyzer = SentimentAnalyzer::getInstance($csv->getData());
 $mostPositive = $sentimentAnalyzer->getDataBy('pos', 'desc')[0];
-$mostNegative = $sentimentAnalyzer->getDataBy('pos', 'asc')[0];
+$mostNegative = $sentimentAnalyzer->getDataBy('neg', 'desc')[0];
 
 echo nl2br("Most positive product description is:\n {$mostPositive['title']} - {$mostPositive['text']} with {$mostPositive['pos']} score\n\n");
 echo nl2br("Most negative description is:\n {$mostNegative['title']} - {$mostNegative['text']} with {$mostNegative['pos']} score");
